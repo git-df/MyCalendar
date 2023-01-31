7085b7e4dd3d58c105ea231000156677a3ee6afe
dawid9f <97540653+dawid9f@users.noreply.github.com>
2023-01-31 11:19:35 +0100

    auth cookie

---
+++ MyCalendar/Pages/Auth/SignUp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCalendar.Pages.Auth
+{
+    public class SignUpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MyCalendar/Program.cs
+++ MyCalendar/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,25 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddPersistance(builder.Configuration);
 builder.Services.AddApplication();
+
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(op => {
+        op.Cookie.Name = "mcAuth";
+        op.LoginPath = "/Auth/SignIn";
+        op.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        op.AccessDeniedPath = "/Home/Index";
+    });
+
+builder.Services.AddAuthorization(op => {
+    //[Authorize(Policy = "User")]
+    op.AddPolicy("User",
+        policy => policy
+        .RequireClaim("Id")
+        .RequireClaim("Email")
+        .RequireClaim("FirstName")
+        .RequireClaim("LastName"));
+});
 
 var app = builder.Build();
 
