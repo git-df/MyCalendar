cedcf7c77908364132e306fbb264b5032ee17205
Dawid Florian <dawidflorian99@gmail.com>
2023-02-02 15:56:53 +0100

    test

---
+++ Application.Test/Auth/SignInTest.cs
@@ -0,0 +1,25 @@
+﻿using Application.Test.Mocks;
+using AutoMapper;
+using Moq;
+using Persistance.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Test.Auth
+{
+    public class SignInTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+
+        public SignInTest()
+        {
+            _userRepositoryMock = UserRepositoryMock.GetUserRepository();
+
+
+        }
+    }
+}
